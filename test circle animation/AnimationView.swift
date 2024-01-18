aa2d475d712ce220bc940dcefee965ec23ef561c
Zoltan Damo <zoltan.damo@gmail.com>
2024-01-18 11:28:15 +0200

    Drawing a single layer

--- test circle animation/AnimationView.swift
+++ test circle animation/AnimationView.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class AnimationView: UIView {
 
+    override class var layerClass: AnyClass {
+        return CAShapeLayer.self
+    }
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -20,10 +24,50 @@ class AnimationView: UIView {
         
         configure()
     }
-    
+      
     
     func configure() {
-        self.backgroundColor = .red
-        self.setNeedsDisplay()
+        self.layer.backgroundColor = UIColor.animationBackground.cgColor
+        var circle: CALayer = .circleLayer
+        
+        self.layer.addSublayer(circle)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        
+    }
+}
+
+extension CALayer {
+    static var circleLayer: CALayer {
+        var layer = CAShapeLayer()
+        layer.backgroundColor = UIColor.animationBackground.cgColor
+        
+        let circle = CGPath(
+            ellipseIn: CGRect(
+                x: 100.0,
+                y: 100.0,
+                width: 20.0,
+                height: 20.00),
+            transform: nil)
+        
+        layer.path = circle
+        layer.strokeColor = UIColor.circle.cgColor
+        layer.fillColor = UIColor.animationBackground.cgColor
+
+        
+        return layer
+    }
+}
+
+extension UIColor {
+    static var animationBackground: UIColor {
+        return UIColor(red: 2.0 / 255.0, green: 3.0 / 255.0, blue: 46.0 / 255.0, alpha: 1.0)
+    }
+    
+    static var circle: UIColor {
+        return UIColor(red: 195.0 / 255.0, green: 196.0 / 255.0, blue: 221.0 / 255.0, alpha: 1.0)
     }
 }
